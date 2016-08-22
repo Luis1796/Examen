073acb64b46729410ac04e7b637886f7fee5395d
Jose Luis <jluiserazod@yahoo.com.mx>
2016-08-22 13:26:37 -0500

    Haciendo tuplas

---
+++ zip.py
@@ -0,0 +1,15 @@
+def zip(lista1,lista2):
+
+	j=0 
+	tupla =[]
+
+	for  i in lista1:  
+		tupla.append([(i,lista2[j])]) 
+		j+=1
+	print(tupla) 
+
+
+lista1=[1,2,3] 
+lista2=['f','h','y']
+zip(lista2,lista1)
+
